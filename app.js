cd4d92a8ac5d2f46c653e619739f197e4f87f90a
Paraskevi <pvflessa@hotmail.com>
2018-07-27 22:06:37 +0300

    Pick random color from the array and when you find it change all squares with given color

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 
+/* 0) SELECT ALL SQUARES
+LOOP THROUGH THEM
+AND THEN ASSIGN ONE COLOR FROM THE "COLORS" ARRAY TO EACH ONES BACKAGRAOUND */
+
+
+/* 1) SELECT A RANDOM COLOR */
+
+
+/* 2) ΕΠΕΙΤΑ ΘΕΛΩ ΤΟ ΧΡΩΜΑ ΠΟΥ ΘΕΛΩ ΝΑ ΜΑΝΤΕΨΕΙ Ο ΧΡΗΣΤΗΣ ΝΑ ΕΜΦΑΝΙΖΕΤΑΙ ΜΕΣΑ ΣΤΟ h2 -> span class ="color-display" */
+
+/* 3) ΘΕΛΩ ΟΤΑΝ ΚΑΝΕΙ ΚΛΙΚ Ο ΧΡΗΣΤΗΣ ΣΕ ΕΝΑ SQUARE ΝΑ ΒΡΙΣΚΕΙ ΤΟ BACKGROUND COLOR ΤΟΥ ΚΑΙ ΝΑ
+ΤΟ ΣΥΓΚΡΙΝΕΙ ΜΕ ΤΟ goalColor */
+
+/* 4) ΘΕΛΩ ΣΕ ΕΝΑ ΣΗΜΕΙΟ ΤΗΣ ΣΕΛΙΔΑΣ ΝΑ ΦΑΙΝΕΤΑ ΕΝΑ ΜΗΝΥΜΑ ΑΝ ΕΙΝΑΙ ΣΩΣΤΟ Η ΛΑΘΟΣ */
+
+/* 5) ΘΕΛΩ ΑΝ ΒΡΩ ΤΟ ΣΩΣΤΟ ΟΛΑ ΤΑ ΤΕΤΡΑΓΩΝΑ ΝΑ ΠΑΡΟΥΝ ΑΥΤΟ ΤΟ ΧΡΩΜΑ ΚΑΘΩΣ ΚΑΙ ΤΟ background του h1 & h2 */
+
+
+
+
+                    // SOLUTION :
+
+
 var colors = [
   "rgb(255, 0, 0)",
   "rgb(255, 255, 0)",
@@ -8,21 +31,38 @@ var colors = [
   "rgb(255, 0, 255)"
 ];
 
+/* 1)= */
 
 
-/* 1) SELECT ALL SQUARES
-LOOP THROUGH THEM
-AND THEN ASSIGN ONE COLOR FROM THE "COLORS" ARRAY TO EACH ONES BACKAGRAOUND */
 
-/* 2) ΕΠΕΙΤΑ ΘΕΛΩ ΤΟ ΧΡΩΜΑ ΠΟΥ ΘΕΛΩ ΝΑ ΜΑΝΤΕΨΕΙ Ο ΧΡΗΣΤΗΣ ΝΑ ΕΜΦΑΝΙΖΕΤΑΙ ΜΕΣΑ ΣΤΟ h2 -> span class ="color-display" */
+function pickColor(){
+  //pick a random number from 0 to 6
 
-/* 3) ΘΕΛΩ ΟΤΑΝ ΚΑΝΕΙ ΚΛΙΚ Ο ΧΡΗΣΤΗΣ ΣΕ ΕΝΑ SQUARE ΝΑ ΒΡΙΣΚΕΙ ΤΟ BACKGROUND COLOR ΤΟΥ ΚΑΙ ΝΑ
-ΤΟ ΣΥΓΚΡΙΝΕΙ ΜΕ ΤΟ goalColor */
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+    //use that number to access the color in the array and return it
+}
+
+
+
+/* 2)= */
+
+var goalColor = pickColor()
+var colorDisplay = document.getElementById("color-display");
+
+colorDisplay.textContent = goalColor;
+
+/* 4)= */
+
+var messageDisplay = document.getElementById("message");
+
+
+
 
 var squares = document.querySelectorAll(".square");
 
 for(var i = 0; i < squares.length; i++ ){
-  // 1)= ΔΙΝΩ ΤΑ ΧΡΩΜΑΤΑ ΣΕ ΚΑΘΕ ΤΕΤΡΑΓΩΝΟ
+  // 0)= ΔΙΝΩ ΤΑ ΧΡΩΜΑΤΑ ΣΕ ΚΑΘΕ ΤΕΤΡΑΓΩΝΟ
    squares[i].style.backgroundColor = colors[i];
    // 3a) = add click listeners to each square
    squares[i].addEventListener('click',function(){
@@ -30,17 +70,27 @@ for(var i = 0; i < squares.length; i++ ){
      var clickedColor = this.style.backgroundColor
      // 3c) = compare color to goalColor
      if (clickedColor === goalColor) {
-        alert('You WON!!!')
+        /* 4)= */
+        messageDisplay.textContent = 'You Won'
+        /* 5)= */
+        changeColors(clickedColor)
      }else{
 
        this.style.backgroundColor = '#232323' //του βάζω το ίδιο χρωμα με το body για να εξαφανιζεται
+      /* 4)= */
+       messageDisplay.textContent = 'Try again'
      }
    })
 }
 
-/* 2)= */
 
-var goalColor = colors[3];
-var colorDisplay = document.getElementById("color-display");
+/* 5)= */
 
-colorDisplay.textContent = goalColor;
+function changeColors(color){
+  //loop through all SQUARES
+
+  for(var i = 0; i < squares.length; i++ ){
+    //change each color to match given color (with clickedColor after the player's success)
+    squares[i].style.backgroundColor = color;
+  }
+}
